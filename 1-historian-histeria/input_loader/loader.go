ee3755e02a11d4488d7716e02b60fafeb270c358
Amine Soufyani <merakigai@Amines-MacBook-Pro.local>
2024-12-12 16:10:27 +0700

    read from input and write output

--- 1-historian-histeria/input_loader/loader.go
+++
@@ -1,5 +0,0 @@
-package loader
-
-func load_input_file(){
-	
-}
--- 1-historian-histeria/main.go
+++ 1-historian-histeria/main.go
@@ -4,35 +4,83 @@ import (
 	"bufio"
 	"fmt"
 	"log"
-	"math/rand"
 	"os"
-	"time"
+	"sort"
+	"strconv"
+	"strings"
 )
 
-func solve() int {
-	return 0
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
+
+func solvePartOne(list1 []int, list2 []int) int {
+
+	count := 0
+
+	sort.Ints(list1)
+	sort.Ints(list2)
+
+	fmt.Println(list1)
+	fmt.Println(list2)
+
+	distances := make([]int, len(list1))
+
+	for i := range list1 {
+		distances[i] = abs(list1[i] - list2[i])
+	}
+
+	fmt.Println(distances)
+
+	for _, v := range distances {
+
+		count += v
+
+	}
+
+	fmt.Println(count)
+	fmt.Println("===============")
+
+	return count
+
 }
 
 func writeToResults(number int) {
-	file, err := os.OpenFile("output.txt", os.O_WRONLY|os.O_CREATE, 0666)
+	str := strconv.Itoa(number)
 
-	if err != nil {
-		fmt.Println("File does not exists or cannot be created")
-		os.Exit(1)
-	}
-	defer file.Close()
-
-	w := bufio.NewWriter(file)
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	i := r.Perm(5)
-	fmt.Fprintf(w, "%v\n", i)
-
+	f, err := os.OpenFile("io/output.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	check(err)
+	defer f.Close()
+	w := bufio.NewWriter(f)
+	w.WriteString(str)
+	w.WriteString(" ")
 	w.Flush()
 
 }
 
+func stringsToIntegers(lines []string) ([]int, error) {
+	integers := make([]int, 0, len(lines))
+	for _, line := range lines {
+		n, err := strconv.Atoi(line)
+		if err != nil {
+			return nil, err
+		}
+		integers = append(integers, n)
+	}
+	return integers, nil
+}
+
 func main() {
-	file, err := os.Open("input.txt")
+	file, err := os.Open("io/input.txt")
 
 	if err != nil {
 		log.Fatal(err)
@@ -42,7 +90,16 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		line := scanner.Text()
+		lists := strings.Split(line, "   ")
+
+		list1, _ := stringsToIntegers(strings.Split(lists[0], ""))
+		list2, _ := stringsToIntegers(strings.Split(lists[1], ""))
+
+		res := solvePartOne(list1, list2)
+
+		writeToResults(res)
+
 	}
 
 	if err := scanner.Err(); err != nil {
